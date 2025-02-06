22521c9335b3c66788009593e55adb1fd2934494
Martynas Klimas <martynas.klimas@mif.stud.vu.lt>
2025-02-06 17:30:14 +0200

    Added particle effects, muzzle flash and bullet points

--- MovementGame/Assets/Scripts/Gun/Gun.cs
+++ MovementGame/Assets/Scripts/Gun/Gun.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -5,38 +6,44 @@ public class Gun : MonoBehaviour
     [Header("Gun settings")]
     [SerializeField] private float damage = 10f;
     [SerializeField] private int maxAmmoMag = 20;
-    [SerializeField] private int maxAmmo = 60;
-    [SerializeField] private float reloadTime = 1f;
-    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float fireDelay = 0.2f;
+    [SerializeField] private float reloadTime = 2f;
+    [SerializeField] private Camera playerCamera;   
+    [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private GameObject hitEffect;
+    //maybe add max weapon ammo and ammo pickups in the future
     private int currAmmoMag;
-    private State state;
+    private float currFireDelay;
+    private bool isReloading = false;
     //particle system, etc.
 
     //todo functions:;
     // fire and deal damage
     // reduce currAmmoMag, when 0, play reload animation
 
-    // Update is called once per frame
-
-    private enum State
-    {
-        Firing,
-        Reloading
-    }
-
     void Awake()
     {
         currAmmoMag = maxAmmoMag;
+        currFireDelay = fireDelay;
     }
 
     void Update()
-    {
-        Fire();
+    {   
+        ResetFireDelay();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+        if(currAmmoMag <= 0 || Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
     }
     
     private void Fire()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && currAmmoMag > 0)
+        if (currAmmoMag > 0 && !isReloading && currFireDelay <= 0f)
         {
             RaycastHit hit;
             if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
@@ -46,9 +53,34 @@ public class Gun : MonoBehaviour
                 if (enemy != null) {
                     enemy.TakeDamage(damage);
                 }
-                state = State.Firing;
+                //particle effects
                 currAmmoMag--;
+                currFireDelay = fireDelay;
+                muzzleFlash.Play();
+                GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impact, 5f);
             }
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        Debug.Log("Reloading");
+        isReloading = true;
+        currAmmoMag = maxAmmoMag;
+        yield return new WaitForSeconds(reloadTime);
+        isReloading = false;
+    }
+
+    private void ResetFireDelay()
+    {
+        if (currFireDelay <= 0f)
+        {
+            currFireDelay = 0f;
+        }
+        else
+        {
+            currFireDelay -= Time.deltaTime;
         }
     }
 }
